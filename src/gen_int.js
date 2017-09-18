e5086f8a4b227ae788e0ed2241835b5fcc0c619f
dotnetCarpenter <jon.ronnenberg@gmail.com>
2017-09-18 22:48:18 +0300

    added repeat

--- src/gen_int.js
+++ src/gen_int.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const identify = x => x
+// const identify = x => x
 
 const gen = (f, start, stop, accu = []) => {
   if(start >= stop) return accu
@@ -8,15 +8,19 @@ const gen = (f, start, stop, accu = []) => {
   return gen(f, start + 1, stop, accu)
 }
 
-const range = (start, stop) => {
-  if(start > stop) return gen(x => -x, 0, 10)
-  else return gen(identify, start, stop)
-
+const range = (start, step, stop) => {
+  if(stop == null) {
+    stop = step
+    step = stop - start
+  }
+  return gen(x => x /*+ step*/, start, stop || step)
 }
 
 const map = (f, data) =>
   gen(x => f(data[x]), 0, data.length)
 
+const repeat = (value, times) => gen(() => value, 0, times).join('')
+
 module.exports = {
-  gen, range, map
+  gen, range, map, repeat
 }
--- test/gen_int.js
+++ test/gen_int.js
@@ -5,6 +5,7 @@ const tap = require('tap')
 const gen = require('../src/gen_int').gen
 const range = require('../src/gen_int').range
 const map = require('../src/gen_int').map
+const repeat = require('../src/gen_int').repeat
 
 tap.test('ranges', t => {
   t.plan(3)
@@ -19,10 +20,11 @@ tap.test('ranges', t => {
   t.match(actual, expected, "should produce an array with float values from 0 to 9")
 
   expected = [ -0, -1, -2, -3, -4, -5, -6, -7, -8, -9 ]
-  actual   = range(0, -10)
+  actual   = gen(x => -x, 0, 10)
   t.match(actual, expected, "should produce an array with integer values from 0 to -9")
 
 })
+
 tap.test('map', t => {
   t.plan(2)
 
@@ -34,4 +36,22 @@ tap.test('map', t => {
   actual   = map(x=>x.name, [{name:'jon'}, {name:'iskra'}])
   t.match(actual, expected)
 })
+
+tap.test('repeat', t => {
+  t.plan(1)
+
+  let expected = 'ani! ani!'
+  let actual   = repeat('ani! ', 2)
+  t.match(actual, expected, "should repeat the string 'ani! ' two times")
+})
+
+/*
+tap.test('reduce', t => {
+  t.plan(1)
+
+  let expected = 22
+  let actual   = reduce(x=>x*2, range(0, 10))
+  t.match(actual, expected, "should produce a value of 22")
+
+})*/
 
