9cfc438c3fa3a2b8c786cd6b43f421ff207e0fc3
dotnetCarpenter <jon.ronnenberg@gmail.com>
2018-07-04 21:00:14 +0200

    updated partial

--- src/partial.js
+++ src/partial.js
@@ -1,14 +1,5 @@
 'use strict'
 
-exports.partial = function(f) {
-  let n = f.length
-
-  // maybe an optimization if(n === 1) return f
-
-  return function partial(...xs) {
-    if(n < xs.length) throw new TypeError((f.name || 'anonymous') + "does not accept " + xs.length + " arguments")
-    return n === xs.length ? f.apply(f, xs) : partial.bind(f, ...xs)
-  }
+exports.partial = function(f, ...xs) {
+  return f.bind(null, ...xs)
 }
-
-
